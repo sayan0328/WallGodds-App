cd4f75802d70358e65f59efa3578e67b3b1cb626
Kaivalya <kaivalya.beohar@gmail.com>
2025-08-17 08:45:41 +0530

    Updated TopAppBar and Hero Section

--- Application/app/src/main/java/com/example/wallgodds/utils/RandomWallpaperGrid.kt
+++ Application/app/src/main/java/com/example/wallgodds/utils/RandomWallpaperGrid.kt
@@ -1,22 +1,35 @@
 package com.example.wallgodds.utils
 
+import android.content.Intent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
+import com.example.wallgodds.R
 import com.example.wallgodds.ui.theme.AppPadding
 import com.example.wallgodds.ui.theme.AppSize
 
@@ -27,10 +40,13 @@ fun RandomWallpaperGrid(
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = AppPadding.MainContentPadding,vertical = AppPadding.Small),
+            .padding(horizontal = AppPadding.MainContentPadding, vertical = AppPadding.Small),
         verticalArrangement = Arrangement.spacedBy(AppPadding.Small)
     ) {
         val chunkedImages = wallpapers.chunked(3)
+        item {
+            HeroSection()
+        }
         items(chunkedImages.size) { index ->
             val images = chunkedImages[index]
             val layoutType = index % 4
@@ -42,6 +58,54 @@ fun RandomWallpaperGrid(
             }
         }
     }
+}
+
+@Composable
+fun HeroSection() {
+    val context = LocalContext.current
+    Box(contentAlignment = Alignment.Center){
+        Image(
+            painter = painterResource(R.drawable.hero_box),
+            contentDescription = null,
+            modifier = Modifier
+                .clickable {
+                    val intent = Intent(
+                        Intent.ACTION_VIEW,
+                        "https://wallgodds.vercel.app/".toUri()
+                    )
+                    context.startActivity(intent)
+                }
+                .clip(RoundedCornerShape(AppSize.MediumCornerRadius))
+                .border(
+                    width = 1.dp, color = Color.LightGray,
+                    RoundedCornerShape(AppSize.MediumCornerRadius)
+                )
+                .aspectRatio(2.5f / 1f),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "WallGodds",
+                fontFamily = FontFamily(Font(R.font.gruppo_regular)),
+                fontSize = AppSize.FontSizeExtraLarge
+            )
+            Text(
+                "Minimal by Design, Inspired by You",
+                fontFamily = FontFamily(Font(R.font.instrument_serif_italic)),
+                fontSize = AppSize.HeroFontSizeMedium
+            )
+            Spacer(modifier = Modifier.height(14.dp))
+            Text(
+                "“Walls That Fit Every Screen — On the Web.”",
+                fontFamily = FontFamily(Font(R.font.habibi)),
+                fontSize = AppSize.HeroFontSizeSmall
+            )
+        }
+    }
+
 }
 
 @Composable
--- Application/app/src/main/java/com/example/wallgodds/utils/TopAppBar.kt
+++ Application/app/src/main/java/com/example/wallgodds/utils/TopAppBar.kt
@@ -1,49 +1,110 @@
 package com.example.wallgodds.utils
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.wallgodds.R
 import com.example.wallgodds.navigation.Routes
-import com.example.wallgodds.ui.theme.AppPadding
 import com.example.wallgodds.ui.theme.AppSize
+import com.example.wallgodds.ui.theme.SoftPink
 
 @Composable
 fun TopAppBar(
     modifier: Modifier = Modifier,
     navController: NavController
-){
-    Column(modifier = modifier) {
+) {
+    var textValue by remember { mutableStateOf("") }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(18.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = AppPadding.Medium),
-            horizontalArrangement = Arrangement.SpaceBetween,
+                .weight(1f)
+                .background(color = SoftPink, RoundedCornerShape(50))
+                .padding(end = 8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
-                painter = painterResource(id = R.drawable.wallgodds_icon),
-                contentDescription = "WallGodds Icon",
-                modifier = Modifier
-                    .size(AppSize.IconMedium)
+                painter = painterResource(R.drawable.wallgodds_icon),
+                contentDescription = null,
+                modifier = Modifier.size(AppSize.IconMedium)
             )
-            Image(
-                painter = painterResource(id = R.drawable.profile_icon),
-                contentDescription = "Profile Icon",
-                modifier = Modifier
-                    .size(AppSize.IconMedium)
-                    .clickable { navController.navigate(Routes.profile_page) }
+            Spacer(modifier = Modifier.width(8.dp))
+            BasicTextField(
+                value = textValue,
+                onValueChange = { textValue = it },
+                modifier = Modifier.weight(1f),
+                textStyle = TextStyle(fontSize = AppSize.placeholderText),
+                singleLine = true,
+                decorationBox = { innerTextField ->
+                    Box {
+                        if (textValue.isEmpty()) {
+                            Text(
+                                "Favourite Wallpapers, categories, etc",
+                                color = Color.Black.copy(0.5f),
+                                maxLines = 1,
+                                fontSize = AppSize.searchBarText
+                            )
+                        }
+                        innerTextField()
+                    }
+                }
             )
+            if (textValue.isNotEmpty()) {
+                Spacer(modifier = Modifier.width(10.dp))
+                Icon(
+                    modifier = Modifier.clickable { textValue = "" },
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = null,
+                    tint = Color.Black.copy(0.5f)
+                )
+            } else {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    tint = Color.Black.copy(0.5f),
+                    modifier = Modifier.size(AppSize.IconSmall)
+                )
+            }
+
         }
+        Spacer(modifier = Modifier.width(12.dp))
+        Image(
+            painter = painterResource(R.drawable.profile_icon),
+            contentDescription = null,
+            modifier = Modifier.size(AppSize.IconMedium)
+                .clickable {
+                    navController.navigate(Routes.profile_page)
+                }
+        )
     }
 }
