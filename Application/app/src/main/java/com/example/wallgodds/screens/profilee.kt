ec57cf46f068c0c15bd56eddd29db7991ec9f4e2
Aviral <aviralpal202@gmail.com>
2026-02-12 20:23:50 +0530

    Made the full profile screen

---
+++ Application/app/src/main/java/com/example/wallgodds/screens/profilee.kt
@@ -0,0 +1,61 @@
+//package com.example.wallgodds.screens
+//
+//import androidx.compose.foundation.Image
+//import androidx.compose.foundation.background
+//import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Row
+//import androidx.compose.foundation.layout.Spacer
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.layout.size
+//import androidx.compose.foundation.layout.width
+//import androidx.compose.foundation.shape.RoundedCornerShape
+//import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.Delete
+//import androidx.compose.material.icons.filled.Star
+//import androidx.compose.material.icons.outlined.BugReport
+//import androidx.compose.material.icons.outlined.ExitToApp
+//import androidx.compose.material.icons.outlined.Nightlight
+//import androidx.compose.material3.Button
+//import androidx.compose.material3.ButtonDefaults
+//import androidx.compose.material3.Icon
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.draw.clip
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.graphics.vector.ImageVector
+//import androidx.compose.ui.layout.ContentScale
+//import androidx.compose.ui.res.painterResource
+//import androidx.compose.ui.text.font.Font
+//import androidx.compose.ui.text.font.FontFamily
+//import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.unit.sp
+//import androidx.navigation.NavController
+//import coil.compose.AsyncImage
+//import com.example.wallgodds.R
+//
+//
+//
+//
+//
+//@Composable
+//fun ProfilePageScreen(navController: NavController) {
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .padding(24.dp),
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//
+//    }
+//}
+//
