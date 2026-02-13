19a5b1c644dc7420b1127e04a90a6af927ef2652
Ankit Singh <ankitsinghak9051@gmail.com>
2026-02-13 22:54:00 +0530

    Refactor: Improve top section layout and alignment on Home page

--- Application/app/src/main/java/com/example/wallgodds/utils/WallpaperGrid.kt
+++ Application/app/src/main/java/com/example/wallgodds/utils/WallpaperGrid.kt
@@ -4,8 +4,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.LazyGridScope
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -14,9 +15,13 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
 fun LazyGridScope.WallpaperGrid(wallpapers: List<Any>, onClick: () -> Unit = {}) {
-    items(wallpapers) { wallpaper ->
+    itemsIndexed(wallpapers) { index, wallpaper ->
+        val startPadding = if (index % 2 == 0) 24.dp else 8.dp
+        val endPadding = if (index % 2 == 0) 8.dp else 24.dp
+
         Box(
             modifier = Modifier
+                .padding(start = startPadding, end = endPadding)
                 .height(320.dp)
                 .clip(RoundedCornerShape(24.dp))
         ) {
@@ -26,7 +31,7 @@ fun LazyGridScope.WallpaperGrid(wallpapers: List<Any>, onClick: () -> Unit = {})
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .fillMaxSize()
-                    .clickable { onClick }
+                    .clickable { onClick() }
             )
         }
     }
